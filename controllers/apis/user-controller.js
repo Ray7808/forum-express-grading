3acab77f3c938995bbe66721bee8a19873a7d6c4
Ray7808 <yuwang7808@gmail.com>
2023-06-01 18:09:11 +0800

    feat: modify api unauthorize response

--- controllers/apis/user-controller.js
+++ controllers/apis/user-controller.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken')
+const userServices = require('../../services/user-services')
 
 const userController = {
   signIn: (req, res, next) => {
@@ -15,6 +16,12 @@ const userController = {
     } catch (err) {
       next(err)
     }
+  },
+  signUp: (req, res, next) => { // 修改這裡
+    userServices.signUp(req, (err, data) => {
+      if (err) return next(err)
+      return res.json({ status: 'success', message: 'Sign up successfully!' })
+    })
   }
 }
 
